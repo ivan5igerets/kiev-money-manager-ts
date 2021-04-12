f9edef692274e8e401f022f82aad386ed0f8f035
ivan5igorets <ivan5igorets@gmai.com>
2021-04-12 22:14:20 +0300

    before mergigng

--- src/views/categories/Categories.vue
+++ src/views/categories/Categories.vue
@@ -17,77 +17,71 @@
         <v-tab-item>
           шо ты 1
 
-          <div class="groups">
+          <v-list>
 
-            <v-list dense>
-              <v-subheader>REPORTS</v-subheader>
+             <!-- list item -->
+            <!-- <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+      
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item> -->
 
-              <v-list-item-group
-                v-model="selectedItem"
-                color="primary"
+            <v-list-group
+              :value="true"
+              prepend-icon="mdi-account-circle"
+              no-action
+            >
+
+              <template v-slot:activator>
+                <v-list-item-title>Users</v-list-item-title>
+              </template>
+
+              <v-list-item
+                v-for="([title, icon], i) in admins"
+                :key="i"
+                link
               >
-                <v-list-item>
-        
-                  <v-list-item-icon>
-                    <v-icon v-text="'mdi-account'"></v-icon>
-                  </v-list-item-icon>
+                <v-list-item-icon>
+                  <v-icon v-text="icon"></v-icon>
+                </v-list-item-icon>
+              
+                <v-list-item-title v-text="title"></v-list-item-title>
 
-                  <v-list-item-content>
-                    <v-list-item-title v-text="'Шо ты там насыпаешь голова?'"></v-list-item-title>
-                  </v-list-item-content>
-                
-                </v-list-item>
+              </v-list-item>
 
-                <v-list-item>
-                  <v-list-group
-                    no-action
-                    sub-group
-                  >
-                    <template v-slot:activator>
-                      <v-list-item-content>
+            </v-list-group>
 
-                        <v-list-item-icon>
-                          <v-icon v-text="'mdi-account'"></v-icon>
-                        </v-list-item-icon>
+            <v-list-group
+              no-action
+            >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title>Actions</v-list-item-title>
+                </v-list-item-content>
+              </template>
+    
+              <v-list-item
+                v-for="([title, icon], i) in cruds"
+                :key="i"
+                link
+              >
+                <v-list-item-title v-text="title"></v-list-item-title>
+    
+                <v-list-item-icon>
+                  <v-icon v-text="icon"></v-icon>
+                </v-list-item-icon>
+              </v-list-item>
+            </v-list-group>
 
-                        <v-list-item-title>Actions</v-list-item-title>
-                      </v-list-item-content>
-                    </template>
-          
-                    <!-- <v-list-item-icon>
-                      <v-icon v-text="'mdi-account'"></v-icon>
-                    </v-list-item-icon> -->
-
-                    <v-list-item-content>
-                      <v-list-item-title v-text="'Шо ты там насыпаешь голова?'"></v-list-item-title>
-                    </v-list-item-content>
-                  </v-list-group>
-                </v-list-item>
-              </v-list-item-group>
-            </v-list>
-
-          </div>
-
-          <br><br><br>
-          
-          <hr>
-
-          <!-- <hr> -->
-
-          <!-- <div class="groups">
-
-          </div> -->
+          </v-list>
 
         </v-tab-item>
         
         <v-tab-item>
           шо ты 2
-          <!-- <v-card
-            color="basil"
-            flat
-          >
-            <v-card-text> шо ты2 </v-card-text>
-          </v-card> -->
+
         </v-tab-item>
       </v-tabs-items>
 
@@ -100,12 +94,49 @@
 <script>
 
 import button_add from '@/components/categories/ButtonAdd'
+import groupsApi from '../../api/groups'
+import categoriesApi from '../../api/categories'
 
 export default {
   
   data() {
     return {
       tab: null,
+      admins: [
+        ['Management', 'mdi-account-multiple-outline'],
+        ['Settings', 'mdi-cog-outline'],
+      ],
+      cruds: [
+        ['Create', 'mdi-plus-outline'],
+        ['Read', 'mdi-file-outline'],
+        ['Update', 'mdi-update'],
+        ['Delete', 'mdi-delete'],
+      ],
+    }
+  },
+
+  mounted() {
+    this.getData()
+  },
+
+  methods: {
+    getData() {
+
+      groupsApi.get()
+      .then(res => {
+        console.log('groups',res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
+
+      categoriesApi.get()
+      .then(res => {
+        console.log('single categories',res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      })
     }
   },
 
