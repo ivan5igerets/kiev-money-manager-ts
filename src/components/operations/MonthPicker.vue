a3ebe881dc21feb4e6c628309d0285411fd62d86
ivan5igorets <ivan5igorets@gmai.com>
2021-04-25 20:24:42 +0300

    fin new bag

--- src/components/operations/MonthPicker.vue
+++ src/components/operations/MonthPicker.vue
@@ -44,18 +44,27 @@ export default {
     },
 
     created() {
-      const tempDate = new Date();
-      this.date = `${tempDate.getFullYear()}-${tempDate.getMonth() + 1}`
+      this.initDate();
     },
 
     methods: {
         changeDate() {
-            // console.log('date', this.date);
             this.isDateOpen = false;
-
-            // тут нужно имитить в рут и передавать дату
             this.$root.$emit('change-date', this.date)
         },
+
+        initDate() {
+          const tempDate = new Date();
+          this.date = `${tempDate.getFullYear()}-${tempDate.getMonth() + 1}`
+        },
+    },
+
+    watch: {
+      $route: function(newUrl) {
+        if (newUrl.name !== 'Diagram' && newUrl.name !== 'OperationHistoryDay') {
+          this.initDate();
+        }
+      }
     },
 
     computed: {
@@ -63,8 +72,6 @@ export default {
             const month = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 
             'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
             const simpleArr = this.date.split('-');
-
-            console.log(this.date);
 
             return month[+simpleArr[1]-1]
         }
@@ -76,7 +83,6 @@ export default {
 .datepicker {
   width: 110px;
   height: 30px;
-//   padding: 0 10px;
 }
 
 </style>
