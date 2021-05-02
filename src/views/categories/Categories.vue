6a58bf8a13176a7b43d42799f617797b523451f9
kirill.shikunov <shikunov998@gmail.com>
2021-05-02 23:33:01 +0300

    Fix small bugs

--- src/views/categories/Categories.vue
+++ src/views/categories/Categories.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="css-categories">
+  <loader v-if="loading" />
+  <div class="css-categories" v-else>
     <v-tabs grow v-model="tab">
       <v-tab>Затраты</v-tab>
       <v-tab>Доход</v-tab>
     </v-tabs>
     <v-divider></v-divider>
-    <loader v-if="loading" />
-    <v-tabs-items v-else v-model="tab">
+    <v-tabs-items v-model="tab">
       <v-tab-item v-if="spending.groups.length || spending.categories.length">
         <v-list v-if="spending.groups.length">
           <v-subheader>Группы</v-subheader>
